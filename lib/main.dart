2b7b72127f00dfa57b75306559bf2ebb999c183b
Marc Dobler <azrael83fr@gmail.com>
2021-01-22 00:01:14 +0100

    start add theme provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:smart_reef/blocs/theme.dart';
 import 'package:smart_reef/screens/wrapper.dart';
 import 'package:smart_reef/services/auth.dart';
 
@@ -13,21 +14,27 @@ void main() async {
 }
 
 class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider<ThemeChanger>(
+      builder: (_) => ThemeChanger(ThemeData.dark()),
+      child: new MaterialAppWithTheme(),
+    );
+  }
+}
+
+class MaterialAppWithTheme extends StatelessWidget {
   // This widget is the root of your application.
 
   @override
   Widget build(BuildContext context) {
+    final theme = Provider.of<ThemeChanger>(context);
+
     return StreamProvider<UserProfile>.value(
       value: AuthService().user,
       child: MaterialApp(
         title: 'Smart Reef',
-        theme: ThemeData(
-          // Need Move in shared folder
-          // Define the default brightness and colors.
-          brightness: Brightness.light,
-          primaryColor: Colors.blue,
-          accentColor: Color.fromRGBO(3, 218, 197, 1),
-        ),
+        theme: theme.getTheme(),
         home: Wrapper(),
       ),
     );
